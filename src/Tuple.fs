195c060f967f0b4f65e702f53b26358ff989a094
John Donnellan <john@codybartfast.com>
2021-05-23 15:59:59 +0100

    Add: basic tuple

--- src/Tuple.fs
+++ src/Tuple.fs
@@ -1,3 +1,47 @@
 ﻿module Tuple
 
+module Tuple =
+    let EPSILON = 0.00001
+    let private wPoint = 1.0
+    let private wVector = 0.0
 
+
+    (* Tuple implementation *)
+    type Tuple = float * float * float * float
+    let raw  x y z w : Tuple = (x, y, z, w)
+    let X (x, _, _, _) = x
+    let Y (_, y, _, _) = y
+    let Z (_, _, z, _) = z
+    let W (_, _, _, w) = w
+    let isPoint t = W t = wPoint
+    let isVector t = W t = wVector
+
+
+    (* Struct Record implementation *)
+    // [<Struct>]
+    // type Tuple =
+    //     { X: float
+    //       Y: float
+    //       Z: float
+    //       W: float }
+
+    // let raw x y z w : Tuple = { X = x; Y = y; Z = z; W = w }
+    // let X t = t.X
+    // let Y t = t.Y
+    // let Z t = t.Z
+    // let W t = t.W
+    // let isPoint t = W t = wPoint
+    // let isVector t = W t = wVector
+
+
+    (* Common implementation *)
+    let point x y z = raw x y z wPoint
+    let vector x y z = raw x y z wVector
+
+    let private floatEqual a b = a - b |> abs |> (>) EPSILON
+
+    let private equal a b =
+        floatEqual (X a) (X b)
+        && floatEqual (Y a) (Y b)
+        && floatEqual (Z a) (Z b)
+        && floatEqual (W a) (W b)
--- test/TupleTests.fs
+++ test/TupleTests.fs
@@ -1,14 +1,35 @@
 module Tests
 
 open Xunit
+open Tuple
 
 [<Fact>]
-let tuple_point_behaviour () =
+let tuple_with_w1_is_a_point () =
     let x, y, z = 4.3, -4.2, 3.1
-    // let a = tuple x y z 1.0
-    // Assert.Equal(x, Tuple.X a)
-    // Assert.Equal(y, Tuple.Y a)
-    // Assert.Equal(z, Tuple.Z a)
-    // Assert.True(isPoint a)
-    // Assert.False(isVector a)
-    ()
+    let a = Tuple.raw x y z 1.0
+    Assert.Equal(x, Tuple.X a)
+    Assert.Equal(y, Tuple.Y a)
+    Assert.Equal(z, Tuple.Z a)
+    Assert.True(Tuple.isPoint a)
+    Assert.False(Tuple.isVector a)
+
+[<Fact>]
+let tuple_with_w0_is_a_vector () =
+    let x, y, z = 4.3, -4.2, 3.1
+    let a = Tuple.raw x y z 0.0
+    Assert.Equal(x, Tuple.X a)
+    Assert.Equal(y, Tuple.Y a)
+    Assert.Equal(z, Tuple.Z a)
+    Assert.False(Tuple.isPoint a)
+    Assert.True(Tuple.isVector a)
+
+[<Fact>]
+let point_creates_tuple_with_w1 () =
+    let x, y, z = 4.3, -4.2, 3.1
+    Assert.Equal(Tuple.raw x y z 1.0, Tuple.point x y z)
+
+[<Fact>]
+let vector_creates_tuple_with_w0 () =
+    let x, y, z = 4.3, -4.2, 3.1
+    Assert.Equal(Tuple.raw x y z 0.0, Tuple.vector x y z)
+
