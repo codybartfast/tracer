4ae510e583cdf0239dd27e51797314df03712bcf
John Donnellan <john@codybartfast.com>
2021-06-07 09:44:08 +0100

    Add: canvas

--- src/lib/Canvas.fs
+++ src/lib/Canvas.fs
@@ -27,20 +27,37 @@ type Canvas (width, height) =
         and set(x, y) v = pixels.[x, y] <- v
     member _.Row y = [| 0..maxX |] |> Array.map (fun x -> pixels.[x, y])
 
-    member this.ToPpm () =
+    member this.WritePpm (write: string -> unit) =
+        let space = " "
+        let writeln str = write str; write newline
+
         let scale depth = ((*) (float depth)) >> ((+) 0.5) >> int
         let clamp low high = (max low) >> (min high)
-        let pixelValues px =
-            [| red px; green px; blue px |]
-            |> Array.map (scale depth >> clamp 0 depth >> string)
-        let rowValues = this.Row >> Array.collect pixelValues
-        let pixelData = [0..maxY] |> List.map (rowValues >> (String.concat " "))
+        let writePixel px =
+            let toStr = scale depth >> clamp 0 depth >> string
+            px |> red |> toStr |> write; write space
+            px |> green |> toStr |> write; write space
+            px |> blue |> toStr |> write
+        let writeRow y =
+            let rec writePixels pixels =
+                match pixels with
+                | [] -> ()
+                | [p] -> writePixel p
+                | p::pxs -> writePixel p; write space; writePixels pxs
+            writePixels (this.Row y |> List.ofArray)
+            write newline
 
-        "P3"
-        ::$"{width} {height}"
-        ::$"{depth}"
-        ::pixelData
-        |> String.concat newline
+        writeln "P3"
+        writeln $"{width} {height}"
+        writeln $"{depth}"
+        [0..maxY] |> List.iter writeRow
+
+    member this.ToPpm () =
+        let sb = StringBuilder ()
+        let write (str: string) = sb.Append(str) |> ignore
+        this.WritePpm write
+        sb.ToString()
+
 
 let canvas width height = Canvas(width, height)
 let pixelAt x y (canvas: Canvas) = canvas.[x, y]
