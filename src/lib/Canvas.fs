fbf1171bff92bf907c6f04081ae42802793db966
John Donnellan <john@codybartfast.com>
2021-06-07 12:11:51 +0100

    Add: (overlooked) PPM line splitting

--- src/lib/Canvas.fs
+++ src/lib/Canvas.fs
@@ -4,56 +4,74 @@ open System.Text
 
 open Tuple
 
-let newline = "\n"
 let black = color 0.0 0.0 0.0
 
-type Canvas (width, height) =
+type Canvas(width, height, initializer) =
     let depth = 255
-
-    let pixels = Array2D.create width height black
+    let pixels = Array2D.init width height initializer
     let maxX = width - 1
     let maxY = height - 1
+    let initializer : (int -> int -> Tuple) = initializer
+
+    new(width, height, color) = Canvas(width, height, (fun _ _ -> color))
+    new(width, height) = Canvas(width, height, black)
 
     member _.Width = width
     member _.Height = height
+
     member _.Pixels =
-        seq{
-            for y in 0..maxY do
-                for x in 0..maxX do
+        seq {
+            for y in 0 .. maxY do
+                for x in 0 .. maxX do
                     yield pixels.[x, y]
         }
-    member _.Item
-        with get(x, y) = pixels.[x, y]
-        and set(x, y) v = pixels.[x, y] <- v
-    member _.Row y = [| 0..maxX |] |> Array.map (fun x -> pixels.[x, y])
 
-    member this.WritePpm (write: string -> unit) =
-        let space = " "
-        let writeln str = write str; write newline
+    member _.Item
+        with get (x, y) = pixels.[x, y]
+        and set (x, y) v = pixels.[x, y] <- v
+
+    member _.Row y =
+        [| 0 .. maxX |]
+        |> Array.map (fun x -> pixels.[x, y])
+
+    member this.WritePpm(write: string -> unit) =
+        let maxLen = 70
+        let mutable lineLen = 0
+        let newline () = write "\n"; lineLen <- 0
+        let writeln str = write str; newline ()
+        let write str = write str; lineLen <- lineLen + str.Length
+        let writeSep () = if lineLen < 67 then write " " else newline ()
 
         let scale depth = ((*) (float depth)) >> ((+) 0.5) >> int
         let clamp low high = (max low) >> (min high)
         let writePixel px =
             let toStr = scale depth >> clamp 0 depth >> string
-            px |> red |> toStr |> write; write space
-            px |> green |> toStr |> write; write space
-            px |> blue |> toStr |> write
+            px |> red |> toStr |> write
+            writeSep ()
+            px |> green |> toStr |> write
+            writeSep ()
+            px |> blue |> toStr  |> write
+
         let writeRow y =
             let rec writePixels pixels =
                 match pixels with
                 | [] -> ()
-                | [p] -> writePixel p
-                | p::pxs -> writePixel p; write space; writePixels pxs
+                | [ p ] -> writePixel p
+                | p :: pxs ->
+                    writePixel p
+                    writeSep ()
+                    writePixels pxs
+
             writePixels (this.Row y |> List.ofArray)
-            write newline
+            newline ()
 
         writeln "P3"
         writeln $"{width} {height}"
         writeln $"{depth}"
-        [0..maxY] |> List.iter writeRow
+        [ 0 .. maxY ] |> List.iter writeRow
 
-    member this.ToPpm () =
-        let sb = StringBuilder ()
+    member this.ToPpm() =
+        let sb = StringBuilder()
         let write (str: string) = sb.Append(str) |> ignore
         this.WritePpm write
         sb.ToString()
@@ -61,5 +79,9 @@ type Canvas (width, height) =
 
 let canvas width height = Canvas(width, height)
 let pixelAt x y (canvas: Canvas) = canvas.[x, y]
-let writePixel x y color (canvas: Canvas) = canvas.[x, y] <- color; canvas
-let toPpm (canvas: Canvas) = canvas.ToPpm ()
+
+let writePixel x y color (canvas: Canvas) =
+    canvas.[x, y] <- color
+    canvas
+
+let canvasToPpm (canvas: Canvas) = canvas.ToPpm()
--- test/CanvasTests.fs
+++ test/CanvasTests.fs
@@ -23,6 +23,7 @@ let writing_pixels_to_canvas () =
     Assert.Equal(red, c.[2, 3])
 
 let lines first last (str: string) =
+    let newline = "\n"
     str.Split(newline).[(first - 1)..(last - 1)]
     |> String.concat newline
 
@@ -43,13 +44,26 @@ let constructing_the_ppm_pixel_data () =
         |> writePixel 0 0 (color 1.5 0.0 0.0)
         |> writePixel 2 1 (color 0.0 0.5 0.0)
         |> writePixel 4 2 (color -0.5 0.0 1.0)
-        |> toPpm
+        |> canvasToPpm
         |> lines 4 6
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let splitting_long_lines_in_ppm_files () =
+    let expected =
+        "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n"
+        + "153 255 204 153 255 204 153 255 204 153 255 204 153\n"
+        + "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n"
+        + "153 255 204 153 255 204 153 255 204 153 255 204 153"
+    let actual =
+        Canvas(10, 2, color 1.0 0.8 0.6)
+        |> canvasToPpm
+        |> lines 4 7
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ppm_files_are_terminated_with_a_newline_characer () =
     let lastChar =
-        (canvas 5 3 |> toPpm).ToCharArray()
+        (canvas 5 3 |> canvasToPpm).ToCharArray()
         |> Array.last
     Assert.Equal('\n', lastChar)
